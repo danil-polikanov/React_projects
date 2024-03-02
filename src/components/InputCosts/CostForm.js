fa3504b1cfdbb4bb82baaea25348e2301ff1d000
Danylo Polikanov <danilo.polikanov@gmail.com>
2024-03-02 21:41:19 +0200

    added handlers

--- src/components/InputCosts/CostForm.js
+++ src/components/InputCosts/CostForm.js
@@ -1,11 +1,11 @@
 import "./CostForm.css"
 import React, {useState} from 'react';
 
-const CostForm=()=>{
+const CostForm=(props)=>{
 
     const[name,setName]=useState('');
     const nameChangeHandler=(event)=>{
-        setName(event.target.value)
+        setName(event.target.value);
     };  
     const[sum,setSum]=useState('');
     const sumChangeHandler=(event)=>{
@@ -16,20 +16,33 @@ const CostForm=()=>{
         setDate(event.target.value)
     };
 
+    const submitHandler=(event)=>{
+        event.preventDefault();
+        const costData={
+            name:name,
+            sun:sum,
+            date:new Date(date)
+        }
+        props.onSaveCostData(costData);
+        setName('');
+        setSum('');
+        setDate('');
+    };
+
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-cost__controls">
             <div className="new-cost__control">
                 <label>Название</label>
-                <input type="text" onChange={nameChangeHandler}/>
+                <input type="text" value={name} onChange={nameChangeHandler}/>
             </div>
             <div className="new-cost__control">
                 <label>Сумма</label>
-                <input type="number" min='0.01' step='0.01' onChange={sumChangeHandler}/>
+                <input type="number" value={sum} min='0.01' step='0.01' onChange={sumChangeHandler}/>
             </div>
             <div className="new-cost__control">
                 <label>Дата</label>
-                <input type="date" min='2019-01-01' steps='2022-12-31' onChange={sumChangeHandler}/>
+                <input type="date" value={date} min='2019-01-01' steps='2022-12-31' onChange={dateChangeHandler}/>
             </div>
             <div className="new-cost__actions">
                 <button type="submit">Добавить Расход</button>
--- src/components/InputCosts/InputCosts.js
+++ src/components/InputCosts/InputCosts.js
@@ -3,9 +3,18 @@ import './InputCosts.css';
 
 
 const InputCosts=() => {
+    const saveCostDataHandler=(inputCostData)=>
+    {
+        console.log(inputCostData)
+        const costData={
+            ...inputCostData,
+            id:Math.random().toString(),
+        }
+        console.log(costData)
+    }
     return (
         <div className='new-cost'>
-        <CostForm></CostForm>
+        <CostForm onSaveCostData={saveCostDataHandler}></CostForm>
         </div>
     );
 }
